3779eb723453ca907534e7b27a33c1bc9087f4d0
forever <andrewtvuong@utexas.edu>
2019-02-13 15:02:22 -0600

    remove old

--- catkin_ws/build/zed-ros-wrapper/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nvidia/catkin_ws/src/zed-ros-wrapper
-# Build directory: /home/nvidia/catkin_ws/build/zed-ros-wrapper
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
